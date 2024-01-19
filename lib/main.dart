32d30a56420d44c47e63b0f16f8a2d0d30a400c0
IFTEKHAR <iftekharahmed728372@gmail.com>
2024-01-19 20:44:52 +0500

    Firebase integration through cli

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:chat_app/Screens/auth/LoginScreen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+
+import 'firebase_options.dart';
 
 // Global object for getting screen size
 late Size mq;
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
